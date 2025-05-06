581b897440d808ec3abe8ef79ad5f31ef364b3f6
s4nshi <karic2233@mail.ru>
2025-05-06 16:07:56 +0500

    new commit

--- TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1cc65d96babdfc1cc745ffcb57761329018df78")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -2,7 +2,7 @@
 
 public class Program
 {
-    public static int SumOfTwoMinimals(int[] arr)
+    public static long SumOfTwoMinimals(int[] arr)
     {
         if (arr == null || arr.Length < 2)
         {
@@ -16,22 +16,24 @@ public class Program
         {
             if (num < min1)
             {
-                min2 = min1; 
-                min1 = num; 
+                min2 = min1;
+                min1 = num;  
             }
             else if (num < min2)
             {
                 min2 = num; 
             }
         }
-
-        return min1 + min2;
+        return (long)min1 + (long)min2;
     }
 
     public static void Main(string[] args)
     {
         var result = SumOfTwoMinimals(new int[] { 4, 0, 3, 19, 492, -10, 1 });
-        Console.WriteLine(result); 
+        Console.WriteLine(result);
+
+        var overflowResult = SumOfTwoMinimals(new int[] { int.MaxValue, int.MaxValue }); //Проверка обработки переполнения при сложении двух максимальных значений типа int.
+        Console.WriteLine(overflowResult); //Вывод: 4294967294 (что соответствует 2 * int.MaxValue)
         Console.ReadKey();
     }
 }
