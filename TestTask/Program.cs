fb222669bbcc915c47ca3d9632491614a33dff2e
s4nshi <karic2233@mail.ru>
2025-06-13 13:11:42 +0500

    new commit

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -19,22 +19,41 @@ public class Program
                 min2 = min1;
                 min1 = num;  
             }
-            else if (num < min2)
+            else if (num < min2 && num != min1) 
             {
                 min2 = num; 
             }
         }
-        return (long)min1 + (long)min2;
+
+        if (min2 == int.MaxValue)
+        {
+            throw new InvalidOperationException("Array must contain at least two distinct elements.");
+        }
+
+        long sum = (long)min1 + (long)min2; 
+
+        return sum;
     }
+
 
     public static void Main(string[] args)
     {
-        var result = SumOfTwoMinimals(new int[] { 4, 0, 3, 19, 492, -10, 1 });
-        Console.WriteLine(result);
+        try
+        {
+            var result = SumOfTwoMinimals(new int[] { 4, 0, 3, 19, 492, -10, 1 });
+            Console.WriteLine(result);
 
-        var overflowResult = SumOfTwoMinimals(new int[] { int.MaxValue, int.MaxValue }); //Проверка обработки переполнения при сложении двух максимальных значений типа int.
-        Console.WriteLine(overflowResult); //Вывод: 4294967294 (что соответствует 2 * int.MaxValue)
-        Console.ReadKey();
+            var overflowResult = SumOfTwoMinimals(new int[] { int.MaxValue, int.MaxValue }); //Проверка обработки переполнения при сложении двух максимальных значений типа int.
+            Console.WriteLine(overflowResult); //Вывод: 4294967294 (что соответствует 2 * int.MaxValue)
+
+
+
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
     }
 }
 
